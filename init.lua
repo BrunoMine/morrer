39d9598dca886626304ffb02bddf5cfbe9b8c43c
BrunoMine <borgesdossantosbruno@gmail.com>
2014-08-16 00:35:18 -0300

    Update init.lua

--- init.lua
+++ init.lua
@@ -4,13 +4,14 @@
 spawn = {x = 100, y = 20, z = 100}
 
 minetest.register_chatcommand("morrer", {
-    description = "Matar Voce",
+    description = "Mata Voce",
     func = function(name, param)
         local player = minetest.get_player_by_name(name)
         local invref = player:get_inventory()
         if invref:is_empty("main") then
             if invref:is_empty("craft") then
                 player:setpos(spawn)
+                player:set_hp(20)
             else
                 player:set_hp(0)
                 minetest.chat_send_player(name, "Caso voce nao esteja no spawn digite /morrer novamente.")
